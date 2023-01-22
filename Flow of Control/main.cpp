eaf47f52a8dc086635b23321ff31c6d5d4ed4330
izal <58200011+rzlahd@users.noreply.github.com>
2023-01-22 05:48:11 -0800

    Update main.cpp

--- Flow of Control/main.cpp
+++ Flow of Control/main.cpp
@@ -2,10 +2,10 @@
 
 main() {
   int result = 0, value  = 1;
-  // keep executing the while as long as val is less than or equal to 10
+  // keep executing the while as long as value is less than or equal to 10
   while (val <= 10)  {
-    result += value;   // assigns sum + val to sum
-    ++value;        // add 1 to val
+    result += value;   // assigns result + value to result
+    ++value;        // add 1 to value
   }
   std::cout<< "Sum of 1 to 10 inclusive is "<<result<<std::endl;
 }
