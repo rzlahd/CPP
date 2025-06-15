414bddd08e17a48be7c4cb02de6d416a2d809b02
izal <58200011+rzlahd@users.noreply.github.com>
2025-06-15 13:07:44 +0700

    Create Portable.bash

---
+++ Build-Command/Portable.bash
@@ -0,0 +1,3 @@
+g++ tes.cpp -o tes.exe -static-libstdc++ -static-libgcc -Wl,-Bstatic -lwinpthread -Wl,-Bdynamic
+echo "Fix DLL Error libwinpthread-1.dll"
+g++ tes.cpp -o tes2.exe -static-libstdc++ -static-libgcc -static
